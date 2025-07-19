40989d5ef3ab9ebd9e27a9e753ec1b6121079a30
Remko Silvis <remko.silvis@mediact.nl>
2025-07-19 17:37:03 +0200

    Add a script to publish

--- github-publish.sh
+++ github-publish.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 # This will push the subtree to the gh-pages branch.
 # Effectively publishing the page as it is in the /public folder.
+# to force it in case of error, this can be used:
+# git push origin `git subtree split --prefix public master`:gh-pages --force
 git subtree push --prefix public origin gh-pages
